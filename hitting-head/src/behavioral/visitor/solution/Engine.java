309f54896f650c7668bb2aaf2d35a21ca0f7a509
Saurabh Chaturvedi <>
2019-10-23 23:57:03 +0530

    [hithead-2020] trying out <Visitor> design pattern implementation .

--- hitting-head/src/behavioral/visitor/solution/Engine.java
+++ hitting-head/src/behavioral/visitor/solution/Engine.java
@@ -2,4 +2,10 @@ package behavioral.visitor.solution;
 
 public class Engine extends CarPart {
 
+	@Override
+	public void acceptCarPartVisitor(CarPartVisitor carPartVisitor) {
+		// TODO Auto-generated method stub
+		super.acceptCarPartVisitor(carPartVisitor);
+	}
+
 }
