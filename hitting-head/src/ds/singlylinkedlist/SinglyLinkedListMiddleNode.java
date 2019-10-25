196a0fd411a132fef5804a6bbea1f97cac934200
Saurabh Chaturvedi <>
2019-10-25 19:48:56 +0530

    [hithead-2020] trying out code to find <Middle> node in the linked list.

---
+++ hitting-head/src/ds/singlylinkedlist/SinglyLinkedListMiddleNode.java
@@ -0,0 +1,45 @@
+package ds.singlylinkedlist;
+
+public class SinglyLinkedListMiddleNode {
+
+	private ListNode head;
+
+	private static class ListNode {
+		private int data;
+		private ListNode next;
+
+		public ListNode(int data) {
+			// TODO Auto-generated constructor stub
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public int middle() {
+		ListNode fastPointer = head;
+		ListNode slowPointer = head;
+		while (fastPointer != null && fastPointer.next != null) {
+			slowPointer = slowPointer.next;
+			fastPointer = fastPointer.next.next;
+		}
+		return slowPointer.data;
+
+	}
+
+	public static void main(String[] args) {
+		SinglyLinkedListMiddleNode linkedList = new SinglyLinkedListMiddleNode();
+		linkedList.head = new ListNode(10);
+		ListNode first = new ListNode(20);
+		ListNode second = new ListNode(30);
+		ListNode third = new ListNode(40);
+		ListNode fourth = new ListNode(50);
+
+		linkedList.head.next = first;
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+
+		int middle = linkedList.middle();
+		System.out.println("The middle element is : " + middle);
+	}
+}
