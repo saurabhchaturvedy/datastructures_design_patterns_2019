54e817859204c2788b733e4d647dfa2b871f46f6
Saurabh Chaturvedi <>
2019-10-25 17:05:40 +0530

    [hithead-2020] bare bones singly linked list .

---
+++ hitting-head/src/ds/singlylinkedlist/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+package ds.singlylinkedlist;
+
+public class SinglyLinkedList {
+
+	private ListNode head;
+
+	private static class ListNode {
+		int data;
+		ListNode next;
+
+		public ListNode(int data) {
+			// TODO Auto-generated constructor stub
+			this.data = data;
+		}
+	}
+}
