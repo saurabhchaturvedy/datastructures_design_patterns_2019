1934886278ccec705c59783853b34c806b600871
Saurabh Chaturvedi <>
2019-10-26 11:11:51 +0530

    [hithead-2020] some change in <Visitor> pattern code.

--- hitting-head/src/behavioral/visitor/solution/Light.java
+++ hitting-head/src/behavioral/visitor/solution/Light.java
@@ -1,11 +1,5 @@
 package behavioral.visitor.solution;
 
 public class Light extends CarPart {
-
-	@Override
-	public void acceptCarPartVisitor(CarPartVisitor carPartVisitor) {
-		// TODO Auto-generated method stub
-		super.acceptCarPartVisitor(carPartVisitor);
-	}
-
+	
 }
--- hitting-head/src/behavioral/visitor/solution/Wheels.java
+++ hitting-head/src/behavioral/visitor/solution/Wheels.java
@@ -1,11 +1,5 @@
 package behavioral.visitor.solution;
 
 public class Wheels extends CarPart {
-
-	@Override
-	public void acceptCarPartVisitor(CarPartVisitor carPartVisitor) {
-		// TODO Auto-generated method stub
-		super.acceptCarPartVisitor(carPartVisitor);
-	}
-
+	
 }
